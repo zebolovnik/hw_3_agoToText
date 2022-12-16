a68de05cf4da2e27ab2f7d9ae1fe9761b9ff3b32
Nik Ze <zebolovnik@yandex.ru>
2022-12-17 09:33:39 +1000

    is fun minutesPronounce & fun hoursPronounce

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,9 @@
 fun main() {
 
-    //    var str = when (a) {
-//        in 0..10 -> "маленькое число"
-//        in 10..20 -> "большое число"
-//        else -> "не могу определить"
-//    }
+    println("был(а) ${agoToText()}")
 
-    var secAgo = 1260
+}
+    var secAgo = 43200
     var minutesAgo = secAgo / 60
     var hoursAgo = minutesAgo / 60
 //    lateinit var minutes: String
@@ -16,24 +13,13 @@ fun main() {
     fun minutesPronounce() =
         when {
             minutesAgo == 1 || minutesAgo % 10 == 1 && minutesAgo != 11 -> "минуту"
-
-//            1, 21, 31, 41, 51 -> "минуту"
-//            (minutesAgo == 1 || minutesAgo % 10 == 1) && !(minutesAgo == 11) -> minutes = "минуту" && hours = "час"
-
-//            2, 3, 4 -> "минуты"
-//            minutesAgo % 10 == in 2..4 ->  minutes = "минуты" && hours = "часа"
             minutesAgo % 10 in 2..4 && minutesAgo !in 12..14 -> "минуты"
-
-
             else -> "минут"
-//            else -> "минут"
-//            else ->  minutes = "минут" && hours = "часов"
         }
 
     fun hoursPronounce() =
         when {
             hoursAgo == 1 || hoursAgo % 10 == 1 && hoursAgo != 11 -> "час"
-//            minutesAgo == 1 || minutesAgo % 10 == 1 && !(minutesAgo == 11) -> "час"
             hoursAgo % 10 in 2..4 && hoursAgo !in 12..14 -> "часа"
             else -> "часов"
         }
@@ -46,6 +32,4 @@ fun main() {
         in 48 * 60 * 60 + 1..72 * 60 * 60 -> "позавчера"
         else -> "давно"
     }
-    println("был(а) ${agoToText()}")
 
-}
